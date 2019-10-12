9a2e682c7cd25d8b7604644d506a5bf46d6c6177
NurKeinNeid <mralexman3000@gmail.com>
2019-10-12 22:35:03 +0200

    base: Add SlatePro Sans font overlay
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -32,6 +32,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontArvoLatoOverlay \
 	FontNotoSerifSourceOverlay \
 	FontRubikRubikOverlay \
+	FontSlateProOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
